d7ee524fc736e6dd55c6ed5cee11bda46804b56b
Brendan <brendanjlee5@gmail.com>
2024-04-16 00:00:10 +0900

    Update formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/Header'
 import Main from './components/Main'
-// import Footer from './components/Footer'
+import Footer from './components/Footer'
 
 import './App.css'
 import { useEffect, useState } from 'react'
@@ -135,7 +135,7 @@ function App() {
         handleCardClick={handleCardClick}
         isLoading={isLoading}
       />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <div className="footer">
-      <p>Footer</p>
+      <p>Github: brendanjlee</p>
     </div>
   );
 }
