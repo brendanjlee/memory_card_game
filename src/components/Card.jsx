520b1e3f837f8ba2c589901296f252286592616e
Brendan <brendanjlee5@gmail.com>
2024-04-15 23:25:32 +0900

    Card styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,10 @@
 import '../styles/Card.css'
-// import cardback from '../assets/cardback.png'
 
 function Card({pokemon, handleCardClick}) {
   return (
     <button onClick={handleCardClick} className="card">
-      <img src={pokemon.spriteUrl} alt='pokemonSprite' className='spriteImg'/>
-      {pokemon.name}
+      <img src={pokemon.spriteUrl} alt='pokemonSprite'/>
+      <span className='pokemon-name'>{pokemon.name}</span>
     </button>
   );
 }
